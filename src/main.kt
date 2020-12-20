57dcf00a1c2851285354cf4181d2659f9f047d2c
Tuna <you@example.com>
2020-12-20 20:33:36 +0800

    nonE

--- src/main.kt
+++ src/main.kt
@@ -1,41 +1,48 @@
-import java.awt.Color
-import java.awt.FlowLayout
+import java.awt.BorderLayout
+import java.awt.Dimension
+import java.awt.Font
+import java.awt.GridLayout
 import javax.swing.*
 
 
 fun main(args: Array<String>) {
-
     //框架
-    val jFrame = JFrame("點餐紀錄")
-    jFrame.setSize(500, 500) //設定視窗大小
+    val jFrame1 = JFrame("待送清單")
+    jFrame1.setSize(500, 1000) //設定視窗大小
     //setLocation //設定顯示位置
     //setBounds //設定顯示位置和視窗大小
-    jFrame.setResizable(false)//設定視窗是否可以拉大或縮小視窗
-    jFrame.contentPane.background= Color.WHITE
-    jFrame.layout=FlowLayout(FlowLayout.CENTER,100,100)
-    //为JFrame顶层容器设置FlowLayout布局管理器
+    jFrame1.setResizable(false)//設定視窗是否可以拉大或縮小視窗
 
-    //中間容器
-    val pane = jFrame.contentPane
+    val jFrame = JFrame("待送清單")
+    jFrame.setSize(500, 750)
+    jFrame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    jFrame.contentPane.layout = BorderLayout()
+    jFrame.setResizable(false)
+
+    val west = JTextArea("West"
+
+
+
+
+    jFrame.add(west, BorderLayout.WEST)
+
+
+    jFrame.isVisible = true
 
     //元件
-    val bten1=JButton("送餐")
-    val radiobutton = JRadioButton("JRadiobutton")
+
+
+    val btnout=JButton("送餐")
+    val Outfd_1 = JRadioButton("JRadiobutton")
+    val Outfd_2 = JRadioButton("JRadiobutton")
+    val Outfd_3 = JRadioButton("JRadiobutton")
     val text = JLabel() //JLabel 標籤文字元件
 
 
     //按键的点击事件
-    bten1.addActionListener{
+    btnout.addActionListener{
         println("已送出")
     }
 
-    text.text = "Hello Swing" //設定標籤顯示的文字
-    pane.add(text) //將元件，加入中間容器中
-    pane.add(bten1)
 
-
-    pane.add(radiobutton)
-
-
-    jFrame.isVisible = true //顯示視窗
 }
